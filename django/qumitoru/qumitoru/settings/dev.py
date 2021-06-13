1df4bb70b8fc33bc6a2486807168f6a45f2f9955
nobu <sp2h5vb9ni@yahoo.co.jp>
2021-06-13 17:37:25 +0900

    [add] create dockerfile for django-container
    - Dockerfileの作成
    - Django projectの作成

---
+++ django/qumitoru/qumitoru/settings/dev.py
@@ -0,0 +1,37 @@
+from .base import *
+
+import boto3
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['0.0.0.0']
+
+# Database
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test',
+        'USER': 'root',
+        'PASSWORD': 'test',
+        'HOST': 'db',
+        'PORT': '3306',
+        'TEST': {
+            'NAME': 'test_test',
+        },
+    }
+}
+
+
+# file upload
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME = os.environ['BUCKET_NAME']
+AWS_DEFAULT_ACL='public-read'
