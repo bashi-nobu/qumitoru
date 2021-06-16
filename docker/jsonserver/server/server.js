d552cfe3965f4240871702ab068a69eacc630a11
bashi-nobu <41232320+bashi-nobu@users.noreply.github.com>
2021-06-16 12:15:59 +0900

    画像アップロード機能の実装 (#11)
    * first commit
    
    * [update] install mdi/font
    
    - mdi/fontのインストール
    
    * [update] add upload page routing
    
    - アップロード画面のルーティング設定を追加
    
    * [add] create django application
    
    - アプリケーション生成(uploader)
    - ルーティング設定
    - UploadFileクラス作成(view.py)
    
    * [update] add storages to  INSTALLED_APPS
    
    * [add] create Upload component
    
    - Uploadページのコンポーネント作成
    - Uploadページのcss作成
    
    * [add] create siderbar.scss
    
    * [add] create PyTest for Upload class
    
    - アップロード処理に関する単体テスト
    
    * [add] create unit&e2e test
    
    - アップロード画面に単体テスト作成
    - アップロード画面に統合テスト作成
    - jsonserverに アップロード関連のレスポンス追加

--- docker/jsonserver/server/server.js
+++ docker/jsonserver/server/server.js
@@ -9,12 +9,12 @@ const cors = require('cors');
 const db = JSON.parse(fs.readFileSync("./data/data.json", "UTF-8"));
 
 server.use(
-    cors({
-        origin: true,
-        credentials: true,
-        preflightContinue: false,
-        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    })
+  cors({
+    origin: true,
+    credentials: true,
+    preflightContinue: false,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  })
 );
 server.options('*', cors());
 
@@ -22,15 +22,28 @@ server.use(middlewares);
 server.use(jsonServer.bodyParser);
 
 server.post("/auth", (req, resp) => {
-	console.log(req.body['username'])
-	if(req.body && req.body['username'] === 'invaliduser'){
-		resp.status(400).json({ "non_field_errors": ["Unable to log in with provided credentials."]});
-	} else {
-		resp.status(200).json({ token: "dummytoken"});
-	}
+  console.log(req.body['username'])
+  if(req.body && req.body['username'] === 'invaliduser'){
+    resp.status(400).json({ "non_field_errors": ["Unable to log in with provided credentials."]});
+  } else {
+    resp.status(200).json({ token: "dummytoken"});
+  }
+});
+server.get("/upload", (req, resp) => {
+  resp.status(200).json({ uploadFilesCount: "0" });
+});
+server.get("/uploaded_file_count", (req, resp) => {
+  resp.status(200).json({ count: "1" });
+});
+server.post("/upload", (req, resp) => {
+  if(Number(req.headers['content-length']) < 10000){
+    resp.status(200).json({ result: "FAIL" });
+  } else {
+    resp.status(200).json({ result: "SUCCESS",  uploadFilesCount: "2"});
+  }
 });
 
 server.use(router);
 server.listen(33000, () => {
-    console.log('JSON Server is running');
+  console.log('JSON Server is running');
 });
--- vue/app/src/router/index.js
+++ vue/app/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Auth from '@/views/Auth'
 import Dashboard from '@/views/Dashboard'
+import Upload from '@/views/Upload'
 
 Vue.use(VueRouter)
 
@@ -17,7 +18,13 @@ const routes = [
     name: 'Dashboard',
     component: Dashboard,
     meta: { title: "ダッシュボード | qumitoru" },
-  }
+  },
+  {
+    path: '/upload',
+    name: 'Upload',
+    component: Upload,
+    meta: { title: "アンケートアップロード | qumitoru" },
+  },
 ]
 
 const router = new VueRouter({
