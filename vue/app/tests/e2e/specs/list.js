6c045b819cf390615f60d0d70dac5cfef6ca6377
nobu <sp2h5vb9ni@yahoo.co.jp>
2021-07-03 13:56:33 +0900

    [add] sidebar responsive test
    - サイドバーの表示メニュー項目のレスポンシブ時の挙動確認テスト

--- vue/app/tests/e2e/specs/list.js
+++ vue/app/tests/e2e/specs/list.js
@@ -69,6 +69,24 @@ module.exports = {
       .click('//button[contains(@class, "submitBtn")]')
       .assert.containsText('//span[contains(@class, "updateResult")]', '更新が完了しました。')
       .end();
+  },
+  'スマホでは非表示': (browser) => {
+    browser
+      .resizeWindow(600, 700)
+      .url(browser.launch_url)
+      // ログイン
+      .useCss()
+      .setValue('input[type=text]', 'validuser')
+      .setValue('input[type=password]', 'validpassword')
+      .click('button[type="button"]')
+      // 1秒待つ
+      .pause(3000)
+      // アップロード画面へ移動
+      .url(browser.launch_url+'/list')
+      // 1秒待つ
+      .pause(1000)
+      .assert.not.visible('.scoreDataListFrame')
+      .end();
   }
 };
 
